5b654fc0ea6780cdf413d34ff57b74064ac39197
Valentin Volkl <valentin.volkl@cern.ch>
2019-09-20 12:16:33 +0200

    [build infrastructure] pull initial setup from fcc-edm

---
+++ .edm4hep-ci.d/compile_and_test.sh
@@ -0,0 +1,9 @@
+
+cd /EDM4HEP
+source init.sh
+mkdir build install
+cd build
+cmake -DCMAKE_INSTALL_PREFIX=../install -DCMAKE_CXX_STANDARD=17 .. && \
+make -j4 && \
+make install && \
+ctest --output-on-failure
